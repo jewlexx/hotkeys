46bf58723f2b3d0c46958ddc95ccce3a256104ab
Juliette Cordor <grassojames5@gmail.com>
2022-08-12 14:20:10 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use inputbot::{
-    KeySequence,
+    BlockInput, KeySequence,
     KeybdKey::{
         self, AKey, BKey, BackquoteKey, CKey, DKey, EKey, FKey, GKey, HKey, IKey, JKey, KKey,
         LControlKey, LKey, LShiftKey, MKey, NKey, OKey, PKey, QKey, RKey, SKey, TKey, UKey, VKey,
@@ -7,6 +7,11 @@ use inputbot::{
     },
 };
 use parking_lot::{const_mutex, Mutex};
+use std::thread;
+
+mod toggle;
+
+use toggle::Toggle;
 
 const KEYS: [inputbot::KeybdKey; 26] = [
     AKey, BKey, CKey, DKey, EKey, FKey, GKey, HKey, IKey, JKey, KKey, LKey, MKey, NKey, OKey, PKey,
@@ -14,27 +19,46 @@ const KEYS: [inputbot::KeybdKey; 26] = [
 ];
 
 static ENABLED: Mutex<bool> = const_mutex(false);
+static JUST_TOGGLED: Mutex<bool> = const_mutex(false);
 
 fn main() {
-    BackquoteKey.bind(|| {
+    BackquoteKey.bind(move || {
         if LControlKey.is_pressed() && LShiftKey.is_pressed() {
             ENABLED.lock().toggle();
+            *JUST_TOGGLED.lock() = true;
         }
     });
 
-    // Bind your caps lock key to a function that starts an autoclicker.
-    for key in KEYS {
-        key.bind(move || {
-            println!("key: {}", key_to_string(&key));
-        })
-    }
+    thread::spawn(move || loop {
+        if *JUST_TOGGLED.lock() {
+            *JUST_TOGGLED.lock() = false;
+            if *ENABLED.lock() {
+                for key in KEYS {
+                    key.bind(move || {
+                        let is_upper = rand::random::<bool>();
+
+                        if is_upper {
+                            LShiftKey.press();
+                        }
+
+                        key.press();
+                        key.release();
+                        println!("pressed key {:?}", key);
+
+                        if is_upper {
+                            LShiftKey.release();
+                        }
+                        // BlockInput::Block
+                    })
+                }
+            } else {
+                for key in KEYS {
+                    key.unbind()
+                }
+            }
+        }
+    });
 
     // Call this to start listening for bound inputs.
     inputbot::handle_input_events();
-}
-
-fn key_to_string(key: &KeybdKey) -> char {
-    let key_name = format!("{key:?}");
-
-    key_name.chars().next().expect("invalid key name")
 }
